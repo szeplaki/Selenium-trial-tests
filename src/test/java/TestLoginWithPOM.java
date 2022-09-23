8185351842cebb4301de3dd22be5a801cc39ec72
Szeplaki <szeplaki.eva1@gmail.com>
2022-09-23 20:36:39 +0200

    refactor TestLoginWithPOM class using enhanced POM

--- src/test/java/TestLoginWithPOM.java
+++ src/test/java/TestLoginWithPOM.java
@@ -1,53 +1,54 @@
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class TestLoginWithPOM {
     WebDriver driver;
     JavascriptExecutor executor;
-
     Actions actions;
 
-    By firstNameField = By.id("firstName");
-    By lastNameField = By.id("lastName");
-    By emailField = By.id("userEmail");
-    By userNumberField = By.id("userNumber");
-
-    By chooseFileButton = By.id("uploadPicture");
-    By day = By.id("dateOfBirthInput");
-    By subjectField = By.id("subjectsInput");
-    By gender = By.xpath("(//input[@name='gender'])[2]");
-    By sport = By.id("hobbies-checkbox-1");
-    By reading = By.id("hobbies-checkbox-2");
-    By music = By.id("hobbies-checkbox-3");
-    By addressField = By.id("currentAddress");
-    By stateField = By.xpath("//*[@id=\"react-select-3-input\"]");
-    By cityField = By.xpath("//*[@id=\"react-select-4-input\"]");
-    By submit = By.id("submit");
-    By successMessage = By.id("example-modal-sizes-title-lg");
+    @FindBy(id = "firstName") WebElement firstNameField;
+    @FindBy(id = "lastName") WebElement lastNameField;
+    @FindBy(id = "userEmail") WebElement emailField;
+    @FindBy(id = "userNumber") WebElement userNumberField;
+    @FindBy(id = "uploadPicture") WebElement chooseFileButton;
+    @FindBy(id = "dateOfBirthInput") WebElement day;
+    @FindBy(id = "subjectsInput") WebElement subjectField;
+    @FindBy(xpath = "(//input[@name='gender'])[2]") WebElement gender;
+    @FindBy(id = "hobbies-checkbox-1") WebElement sport;
+    @FindBy(id = "hobbies-checkbox-2") WebElement reading;
+    @FindBy(id = "hobbies-checkbox-3") WebElement music;
+    @FindBy(id = "currentAddress") WebElement addressField;
+    @FindBy(xpath = "//*[@id=\"react-select-3-input\"]") WebElement stateField;
+    @FindBy(xpath = "//*[@id=\"react-select-4-input\"]") WebElement cityField;
+    @FindBy(id = "submit") WebElement submit;
+    @FindBy(id = "example-modal-sizes-title-lg") WebElement successMessage;
 
     public TestLoginWithPOM(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
         this.executor = (JavascriptExecutor) this.driver;
     }
 
     public void setFirstName(String firstName) {
-        driver.findElement(firstNameField).sendKeys(firstName);
+        firstNameField.sendKeys(firstName);
     }
     public void setLastName(String lastName) {
-        driver.findElement(lastNameField).sendKeys(lastName);
+        lastNameField.sendKeys(lastName);
     }
     public void setEmail(String email) {
-        driver.findElement(emailField).sendKeys(email);
+        emailField.sendKeys(email);
     }
     public void setGender() {
-        executor.executeScript("arguments[0].click()", driver.findElement(gender));
+        executor.executeScript("arguments[0].click()", gender);
     }
     public void setPhoneNumber(String phoneNumber) {
-        driver.findElement(userNumberField).sendKeys(phoneNumber);
+        userNumberField.sendKeys(phoneNumber);
     }
     public void setDate(String date) {
-        driver.findElement(day).click();
+        day.click();
 
         actions = new Actions(driver);
         actions.sendKeys(Keys.HOME).build().perform();
@@ -59,43 +60,43 @@ public class TestLoginWithPOM {
         actions.keyUp(Keys.LEFT_SHIFT);
         actions.build().perform();
 
-        driver.findElement(day).sendKeys(date);
-        driver.findElement(day).sendKeys(Keys.ENTER);
+        day.sendKeys(date);
+        day.sendKeys(Keys.ENTER);
 
     }
     public void setSubject(String subject) {
-        executor.executeScript("arguments[0].click()", driver.findElement(subjectField));
-        driver.findElement(subjectField).sendKeys(subject);
-        driver.findElement(subjectField).sendKeys(Keys.ENTER);
+        executor.executeScript("arguments[0].click()", subjectField);
+        subjectField.sendKeys(subject);
+        subjectField.sendKeys(Keys.ENTER);
     }
 
     public void setSportAsHobby() {
-        executor.executeScript("arguments[0].click()", driver.findElement(sport));
+        executor.executeScript("arguments[0].click()", sport);
     }
     public void setReadingAsHobby() {
-        executor.executeScript("arguments[0].click()", driver.findElement(reading));
+        executor.executeScript("arguments[0].click()", reading);
     }
     public void setMusicAsHobby() {
-        executor.executeScript("arguments[0].click()", driver.findElement(music));
+        executor.executeScript("arguments[0].click()", music);
     }
     public void uploadFile(String route) {
-        driver.findElement(chooseFileButton).sendKeys(route);
+        chooseFileButton.sendKeys(route);
     }
     public void setAddress(String address) {
-        driver.findElement(addressField).sendKeys(address);
+        addressField.sendKeys(address);
     }
     public void setState(String state) {
-        driver.findElement(stateField).sendKeys(state);
-        driver.findElement(stateField).sendKeys(Keys.ENTER);
+        stateField.sendKeys(state);
+        stateField.sendKeys(Keys.ENTER);
     }
     public void setCity(String city) {
-        driver.findElement(cityField).sendKeys(city);
-        driver.findElement(cityField).sendKeys(Keys.ENTER);
+        cityField.sendKeys(city);
+        cityField.sendKeys(Keys.ENTER);
     }
     public void clickSubmit() {
-        executor.executeScript("arguments[0].click()", driver.findElement(submit));
+        executor.executeScript("arguments[0].click()", submit);
     }
     public void checkSuccess() {
-        Assertions.assertEquals("Thanks for submitting the form", driver.findElement(successMessage).getText());
+        Assertions.assertEquals("Thanks for submitting the form", successMessage.getText());
     }
 }
